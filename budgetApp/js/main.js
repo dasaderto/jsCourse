c71b2818bce3ff2c8bfe890bfbcc46f73d742e4b
Nikita Esipov <dasaderto@gmail.com>
2019-04-07 23:59:37 +0300

    7 занятие

---
+++ budgetApp/js/main.js
@@ -0,0 +1,17 @@
+let startCalculation = document.querySelector('#start');
+
+let budgetValue = document.querySelector('.budget-value');
+let daybudgetValue = document.querySelector('.daybudget-value');
+let levelValue = document.querySelector('.level-value');
+let expensesValue = document.querySelector('.expenses-value');
+let optionalExpensesValue = document.querySelector('.optionalexpenses-value');
+let incomeValue = document.querySelector('.income-value');
+let monthsavingsValue = document.querySelector('.monthsavings-value');
+let yearsavingsValue = document.querySelector('.yearsavings-value');
+
+let expensesItems = document.querySelectorAll('.expenses-item');
+
+let approveExpenses = document.querySelector('.expenses-item-btn');
+let calculateBudget = document.querySelector('.count-budget-btn');
+
+let optionalExpenses = document.querySelectorAll('.optionalexpenses-item');
